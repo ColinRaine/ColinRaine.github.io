839497167e8155b497b2c456f240c12396081353
Colin <>
2019-07-16 18:08:01 -0500

    update

--- dbleqp/script.js
+++ dbleqp/script.js
@@ -1,16 +1,33 @@
 const attr1 = document.querySelector('.attr1');
 const weight1 = document.querySelector('.weight1');
-
-const	result = document.querySelector('.result');
 const submit = document.querySelector('.submit');
-
 const table = document.querySelector('.table');
-
-let resetTable;
+const result = document.querySelector('.result');
+let index = 1;
 
 function weightAvg() {
-	alert('im a placeholder');
+	let value1 = Number(attr1.value);
+	let value2 = Number(weight1.value)/100;
+	let sum = (value1 * value2)/2;
+	result.value = sum;
+	appendTable(sum);
 }
+
+function appendTable(sum) {
+	let tr = document.createElement('tr');
+	let cell = document.createElement('td');
+	let cell2 = document.createElement('td');
+	let num = sum;
+
+	cell.textContent = index;
+	table.appendChild(tr);
+	table.appendChild(cell);
+	index++;
+	cell2.textContent = sum;
+	table.appendChild(cell2);
+}
+
+submit.addEventListener('click', weightAvg);
 
 
 
