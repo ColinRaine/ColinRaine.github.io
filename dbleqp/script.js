f2ddcc523e4b112a5901cf0c86c30a38013f2c3a
Colin <>
2019-07-16 15:20:58 -0500

    update

--- dbleqp/script.js
+++ dbleqp/script.js
@@ -1,2 +1,16 @@
+const attr1 = document.querySelector('.attr1');
+const weight1 = document.querySelector('.weight1');
+
+const	result = document.querySelector('.result');
+const submit = document.querySelector('.submit');
+
+const table = document.querySelector('.table');
+
+let resetTable;
+
+function weightAvg() {
+	alert('im a placeholder');
+}
+
 
 
