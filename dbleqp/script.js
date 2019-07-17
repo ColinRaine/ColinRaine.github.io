e4b4dbbe73797f172f8c9c627b6596ece56e837e
Colin <>
2019-07-16 23:06:18 -0500

    update

--- dbleqp/script.js
+++ dbleqp/script.js
@@ -1,14 +1,16 @@
-const attr1 = document.querySelector('.attr1');
-const weight1 = document.querySelector('.weight1');
+const attr = [document.querySelector('.attr1'), document.querySelector('.attr2'),document.querySelector('.attr3')];
+const weight = [document.querySelector('.weight1'),document.querySelector('.weight2'),document.querySelector('.weight3')];
 const submit = document.querySelector('.submit');
 const table = document.querySelector('.table');
 const result = document.querySelector('.result');
 let index = 1;
 
 function weightAvg() {
-	let value1 = Number(attr1.value);
-	let value2 = Number(weight1.value)/100;
-	let sum = (value1 * value2)/2;
+	let value1 = Number(attr[0].value) * (Number(weight[0].value)/100);
+	let value2 = Number(attr[1].value) * (Number(weight[1].value)/100);
+	let value3 = Number(attr[2].value) * (Number(weight[2].value)/100);
+
+	let sum = value1+value2+value3;
 	result.value = sum;
 	appendTable(sum);
 }
